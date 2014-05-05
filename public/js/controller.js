6aa73f62322aaca24393348e3d4e9e2dd8960939
Alex Chantavy <chantavy@gmail.com>
2014-05-04 20:41:41 -1000

    before i attempt to try using ng-table instead

--- public/js/controller.js
+++ public/js/controller.js
@@ -33,20 +33,33 @@ uhfind.controller('uhfindCtrl', function($scope, $http) {
     grid: undefined
   };
 
+  angular.forEach($scope.courses, function(row) {
+    row.concatTime = function() {
+      return this.mtgTime[0].loc + ': ' + this.mtgTime[0].days + ' -- ' + this.mtgTime[0].start + '-' + this.mtgTime[0].end;
+    };
+  });
+
 
   var colDefs = [
     {field: 'course', displayName: 'Course', headerCellTemplate: 'partials/filterHead'},
+    {field: 'title', displayName: 'Title', headerCellTemplate: 'partials/filterHead', width: '20%'},
     {field: 'credits', displayName: 'Credits', headerCellTemplate: 'partials/filterHead'},
-    {field: 'crn', displayName: 'CRN', headerCellTemplate: 'partials/filterHead'},
-    {field: 'genEdFocus', displayName: 'GenEd/Focus Reqs', headerCellTemplate: 'partials/filterHead'},
+    {field: 'genEdFocus', displayName: 'Reqs Met', headerCellTemplate: 'partials/filterHead'},
+    {field: 'mtgTime[0].loc', displayName: 'Location1', headerCellTemplate: 'partials/filterHead'},
+    {field: 'mtgTime[0].days', displayName: 'Days', headerCellTemplate: 'partials/filterHead'},
+    {field: 'mtgTime[0].start', displayName: 'Start1', headerCellTemplate: 'partials/filterHead'},
+    {field: 'mtgTime[0].end', displayName: 'End1', headerCellTemplate: 'partials/filterHead'},
     {field: 'instructor', displayName: 'Instructor', headerCellTemplate: 'partials/filterHead'},
-    {field: 'mtgTime', displayName: 'Meeting Time', headerCellTemplate: 'partials/filterHead'},
     {field: 'seatsAvail', displayName: 'Seats Avail', headerCellTemplate: 'partials/filterHead'},
-    {field: 'sectionNum', displayName: 'Section Num', headerCellTemplate: 'partials/filterHead'},
-    {field: 'title', displayName: 'Title', headerCellTemplate: 'partials/filterHead'},
-    {field: 'waitAvail', displayName: 'Wait Avail', headerCellTemplate: 'partials/filterHead'},
-    {field: 'waitListed', displayName: 'Wait List', headerCellTemplate: 'partials/filterHead'}
+   // {field: 'waitAvail', displayName: 'Wait Avail', headerCellTemplate: 'partials/filterHead'},
+   // {field: 'waitListed', displayName: 'Wait List', headerCellTemplate: 'partials/filterHead'},
+    {field: 'crn', displayName: 'CRN', headerCellTemplate: 'partials/filterHead'},
+    {field: 'sectionNum', displayName: 'Sect.', width: '5%'} //headerCellTemplate: 'partials/filterHead'},
+
   ];
+
+
+
 
   $scope.ngGridOptions = { 
     data: 'courses',

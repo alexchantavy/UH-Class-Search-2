32352f925b3c506d9ae211f0e9f4af418b82cbdf
Alex Chantavy <chantavy@gmail.com>
2014-05-27 19:04:15 -1000

    Update grab.sh to both run phantom _AND_ save to MongoDB

--- backend/grab.sh
+++ backend/grab.sh
@@ -1,1 +1,7 @@
+#!/bin/sh
+
+# grab course data from UH, save to data.json
 phantomjs --ssl-protocol=tlsv1 --ignore-ssl-errors=true run-uhfind-phntm.js
+
+# save contents of data.json to mongodb
+node run-uhfind-save.js

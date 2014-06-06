ef3de8c45041e7c383c8c8f6cd7935c59bc173a9
Alex Chantavy <chantavy@gmail.com>
2014-06-05 22:50:51 -1000

    fiddling around with test files

--- test/specs/uhfind_spec.js
+++ test/specs/uhfind_spec.js
@@ -1,19 +1,14 @@
 var UHFind = require('../backend/uhfind.js');
 var uhfind = new UHFind();
 
-
-describe('uhfind object', function() {
-	expectHasProperty(uhfind, 'baseUrl');
-	expectHasProperty(uhfind, 'departments');
-	expectHasFunction(uhfind, 'fetchDeptCourses');
-});
-
-describe('data scraped from uh catalog', function() {
-	it ('should be non-null, non-empty data', function() {
-    uhfind.fetchDeptCourses('ICS', function(result){
+describe('fetchDeptCourses()', function() {
+  it('should return data as expected', function(done) {
+    uhfind.fetchDeptCourses('ICS', function(result) {
+      jasmine.log(JSON.stringify(result));
       expect(result).not.to.be.null;
       expect(result).to.be.an.object;
       expect(result.length > 0).to.equal(true);
+      done();
 	  });
   });
 });

7c12bf0f46436ac1539cb895b811b78e81de4cb1
Alex Chantavy <chantavy@gmail.com>
2014-11-27 01:26:51 -0500

    added table of departments by campus

--- backend/run-uhfind-phntm.js
+++ backend/run-uhfind-phntm.js
@@ -6,19 +6,131 @@ grab.sh.  also, this won't run from node; it'll only
 work with phantomjs.
 
 */
-
-var async   = require('async'),
 //remember that phantom's `fs` is NOT node's `fs`.
-    fs      = require('fs'),
-    UHFind  = require('./uhfind.js');
-    uhfind  = new UHFind();
+var async   = require('async')
+,   fs      = require('fs')
+,   UHFind  = require('./uhfind.js')
+,   uhfind  = new UHFind();
+
+
+var uhSystem = [{
+  campus: 'HAW',
+  departments: ['ACC', 'AJ', 'AG', 'ANTH', 'CAD', 'AEC', 'ART', 'ASAN', 'ASTR', 
+    'ABRP', 'AMT', 'BIOC', 'BIOL', 'BLPR', 'BOT', 'BUS', 'BUSN', 'CARP', 'CHEM',
+    'CENT', 'CULN', 'DNCE', 'DIMC', 'ECOM', 'ECED', 'ECON', 'EIMT', 'ETRO', 
+    'ESOL', 'ENG', 'ESL', 'ENT', 'FAMR', 'FIRE', 'GEOG', 'GG', 'HAW', 'HWST', 
+    'HIST', 'HOST', 'HD', 'HSER', 'HUM', 'ITS', 'ICS', 'IS', 'JPNS', 'LING', 
+    'MWIM', 'MKT', 'MATH', 'MICR', 'NURS', 'OCN', 'PHRM', 'PHIL', 'PHYS', 'PSY',
+    'QM', 'REL', 'SCI', 'SSCI', 'SOC', 'SPCO', 'SUBS', 'WS', 'ZOOL']
+},
+{
+  campus: 'HON', 
+  departments: ['ACC', 'AJ', 'AERO', 'AS', 'AG', 'AMST', 'ANTH', 'CAD', 'AEC', 
+    'ART', 'ASAN', 'ASTR', 'ABRP', 'AMT', 'BIOC', 'BIOL', 'BLPR', 'MARR', 'BOT',
+    'CARP', 'CHEM', 'AVIT', 'CA', 'CENT', 'CMGT', 'COSM', 'DISL', 'ECED', 
+    'EALL', 'ECON', 'EIMT', 'ENG', 'ESL', 'FAMR', 'FT', 'FIRE', 'FSHN', 'GEOG',
+    'GG', 'HAW', 'HWST', 'HIST', 'HUM', 'IED', 'IEDD', 'ICS', 'IS', 'JPN', 
+    'JOUR', 'KLS', 'KOR', 'LING', 'MATH', 'MET', 'MICR', 'MSL', 'MUS', 'MELE',
+    'OESM', 'OCN', 'PHIL', 'PHYS', 'PHYL', 'POLS', 'PSY', 'RAC', 'REL', 'SCI', 
+    'SMP', 'SSCI', 'SOSE', 'SW', 'SOC', 'SPAN', 'SP', 'WELD', 'WS', 'ZOOL']
+},
+{
+  campus: 'KAU',
+  departments: ['ACC', 'AG', 'ANTH', 'CAD', 'AEC', 'ART', 'ABRP', 'AMT', 'BIOL',
+    'BOT', 'BUS', 'BLAW', 'BUSN', 'CARP', 'CHEM', 'CULN', 'ECOM', 'ECON', 'ED', 
+    'ELEC', 'ETRO', 'ENG', 'ELI', 'ENT', 'FENG', 'GEOG', 'HAW', 'HWST', 'HLTH', 
+    'HPER', 'HIST', 'HORT', 'HOST', 'ICS', 'IS', 'JPNS', 'JOUR', 'LING', 'MGT', 
+    'MARE', 'MATH', 'MEDA', 'MICR', 'MUS', 'NURS', 'OCN', 'PHIL', 'PHYS', 'PBT',
+    'POLS', 'PSY', 'REL', 'SMKT', 'SCI', 'SSCI', 'SOC', 'SPAN', 'SP', 'SSM', 
+    'WELD', 'ZOOL']
+},
+{
+  campus: 'LEE',
+  departments: ['ACC', 'AG', 'AMST', 'ANTH', 'ART', 'ASAN', 'ASTR', 'AMT', 
+    'BIOC', 'BIOL', 'BOT', 'BUS', 'BLAW', 'BUSN', 'CHEM', 'CHN', 'CE', 'COM', 
+    'CULN', 'DNCE', 'DMED', 'ECOM', 'ECON', 'ED', 'EE', 'ENG', 'ESL', 'ELI', 
+    'FAMR', 'FIN', 'FSHN', 'FR', 'GEOG', 'GG', 'HAW', 'HWST', 'HLTH', 'HIT', 
+    'HIST', 'HORT', 'HOST', 'HSER', 'ICS', 'IS', 'JPNS', 'KOR', 'LSK', 'LING', 
+    'MGT', 'MKT', 'MATH', 'ME', 'MICR', 'MUS', 'OCN', 'PACS', 'PHIL', 'PHYS', 
+    'PBT', 'POLS', 'PSY', 'QM', 'REL', 'SSCI', 'SOC', 'SPAN', 'SP', 'TAG', 
+    'TVPR', 'THEA', 'TIM', 'WS', 'ZOOL']
+},
+{
+  campus: 'HIL',
+  departments: ['ACC', 'AJ', 'AGBU', 'AGEC', 'AGEN', 'AG', 'ANSC', 'ANTH', 
+    'AQUA', 'ART', 'ASTR', 'BIOL', 'BUS', 'CHEM', 'CHNS', 'COM', 'CS', 'DNCE', 
+    'DRAM', 'ECON', 'ED', 'KED', 'ENGR', 'ENG', 'ESL', 'ENTO', 'ENSC', 'FIL', 
+    'FIN', 'FOR', 'GEOG', 'GEOL', 'HAW', 'KHAW', 'HWST', 'KHWS', 'HIST', 'HON', 
+    'HORT', 'KIND', 'IS', 'JPNS', 'JPST', 'KES', 'LANG', 'LING', 'MGT', 'MARE', 
+    'MKT', 'MATH', 'MSL', 'MUS', 'NRES', 'NURS', 'PHPS', 'PHPP', 'PHIL', 'PHYS',
+    'PPTH', 'POLS', 'PSY', 'QBA', 'SOC', 'SOIL', 'SPAN', 'TOUR', 'CBES', 'WS']
+},
+{
+  campus: 'MAN', 
+  departments: ['ACM', 'ACC', 'AS', 'AMST', 'ANAT', 'ANSC', 'ANTH', 'ARAB', 
+    'ARCH', 'ART', 'ASAN', 'ASTR', 'BIOC', 'BE', 'BIOL', 'BIOM', 'BOT', 'BUS', 
+    'BLAW', 'CAM', 'CMB', 'CHAM', 'CHEM', 'CHN', 'CEE', 'COM', 'CIS', 'CSD', 
+    'COMG', 'EDCS', 'DNCE', 'DH', 'DRB', 'DIS', 'EALL', 'ECON', 'EDEA', 'EDEF', 
+    'EDEP', 'ETEC', 'EE', 'ENGR', 'ENG', 'ESL', 'ELI', 'ES', 'FMCH', 'FAMR', 
+    'FDM', 'FIL', 'FIN', 'FSHN', 'FR', 'GEOG', 'GG', 'GERI', 'GER', 'GHPS', 
+    'GRK', 'HAW', 'HWST', 'HNDI', 'HIST', 'HON', 'HRM', 'ILO', 'IP', 'IND', 
+    'ITM', 'ICS', 'ITE', 'IS', 'CUL', 'ITAL', 'JPN', 'JOUR', 'KRS', 'KOR', 
+    'LLEA', 'LATN', 'LAIS', 'LAW', 'LWEV', 'LWJT', 'LWLW', 'LWPA', 'LIS', 
+    'LING', 'MGT', 'MAO', 'MBIO', 'MKT', 'MATH', 'ME', 'MDED', 'MEDT', 'MED', 
+    'MET', 'MICR', 'MSL', 'MCB', 'MBBE', 'MUS', 'NHH', 'NREM', 'NURS', 'OBGN', 
+    'ORE', 'OCN', 'PACS', 'PATH', 'PACE', 'PED', 'PERS', 'PHRM', 'PHIL', 'PHYS',
+    'PHYL', 'PEPS', 'POLS', 'PORT', 'PSTY', 'PSY', 'PUBA', 'PH', 'PPC', 'RE', 
+    'REL', 'RUS', 'SAM', 'SNSK', 'SLS', 'SOCS', 'SW', 'SOC', 'SPAN', 'SPED', 
+    'SURG', 'TAHT', 'THAI', 'THEA', 'TONG', 'TI', 'TIM', 'TRMD', 'TPSS', 'PLAN'
+    'URDU', 'VIET', 'WS', 'ZOOL']
+}, 
+{
+  campus: 'KAP',
+  departments: ['ACC', 'ASL', 'AMST', 'ANTH', 'ART', 'ASAN', 'ASTR', 'BIOC', 
+    'BIOL', 'BOT', 'BUS', 'BLAW', 'CHEM', 'CHNS', 'CE', 'COM', 'CULN', 'DNCE', 
+    'DEAF', 'DENT', 'EALL', 'EBUS', 'ECON', 'ED', 'EE', 'EMT', 'ESOL', 'ENG', 
+    'ESL', 'ENT', 'ES', 'ESS', 'FAMR', 'FIL', 'FSHE', 'FR', 'GEOG', 'GG', 'HAW',
+    'HWST', 'HLTH', 'HIST', 'HOST', 'ITS', 'ICS', 'IS', 'JPNS', 'JOUR', 'KOR', 
+    'LAW', 'LING', 'MGT', 'MKT', 'MATH', 'ME', 'MEDA', 'MLT', 'MICR', 'MICT', 
+    'MUS', 'NURS', 'OTA', 'OCN', 'PACS', 'PHRM', 'PHIL', 'PTA', 'PHYS', 'PHYL', 
+    'POLS', 'PSY', 'RAD', 'REL', 'RESP', 'SLT', 'SOCS', 'SSCI', 'SOC', 'SPAN', 
+    'SP', 'THEA', 'WS', 'ZOOL']
+},
+{
+  campus: 'MAU',
+  departments: ['ACC', 'AJ', 'AG', 'ANTH', 'CAD', 'AEC', 'ART', 'ASTR', 'ABRP', 
+    'AMT', 'BIOL', 'BLPR', 'BOT', 'BUS', 'BLAW', 'BUSN', 'CARP', 'CHEM', 'COM', 
+    'CASE', 'CULN', 'DENT', 'DH', 'DRAM', 'ECED', 'EALA', 'ECON', 'ELEC', 
+    'ETRO', 'ENRG', 'ENG', 'FAMR', 'FT', 'FIL', 'FIN', 'FSHN', 'GIS', 'GEOG', 
+    'HAW', 'HWST', 'HLTH', 'HIST', 'HOST', 'HSER', 'HUM', 'ILO', 'ICS', 'IS', 
+    'JPNS', 'JOUR', 'LSK', 'LING', 'MAIN', 'MGT', 'MKT', 'MATH', 'MICR', 'MUS',
+    'NURS', 'OCN', 'PACS', 'PHRM', 'PHIL', 'PHYS', 'POLS', 'PSY', 'REL', 'SCI', 
+    'SOC', 'SPAN', 'SP', 'SSM', 'WELD', 'ZOOL']
+},
+{
+  campus: 'WOA',
+  departments: ['ACC', 'AS', 'ANTH', 'APSC', 'ART', 'BIOL', 'BOT', 'BUSA', 
+    'CHEM', 'CENT', 'CM', 'ECED', 'ECON', 'EDUC', 'EDEF', 'ETEC', 'EDEE', 'ENG',
+    'FIN', 'GEOG', 'GEOL', 'HPST', 'HAW', 'HWST', 'HIST', 'HUM', 'ISA', 'ITS', 
+    'ICS', 'MGT', 'MATH', 'MET', 'MSL', 'MUS', 'OCN', 'PACS', 'PHIL', 'PHYS', 
+    'POLS', 'PSY', 'PUBA', 'SAM', 'SSCI', 'SOC', 'SPED', 'SP', 'SCFS']
+},
+{
+  campus: 'WIN',
+  departments: ['ACC', 'AG', 'ANSC', 'ANTH', 'AQUA', 'ART', 'ASTR', 'BIOC', 
+    'BIOL', 'BOT', 'BUS', 'CHEM', 'ECON', 'EE', 'ENG', 'FAMR', 'GEOG', 'GG', 
+    'HAW', 'HWST', 'HLTH', 'HIST', 'ICS', 'IS', 'JPNS', 'JOUR', 'LING', 'MATH', 
+    'MET', 'MICR', 'MUS', 'OCN', 'PACS', 'PHRM', 'PHIL', 'PHYS', 'POLS', 'PSY', 
+    'REL', 'SSCI', 'SW', 'SOC', 'SPAN', 'SP', 'THEA', 'ZOOL']
+}];
 
 function getClasses(dept, callback) {
   uhfind.fetchDeptCourses(dept, function(err, result) {
-    if (err)
+    if (err) {
       callback(err);
-    else
+    } else {
       callback(null, result);  
+    }
   });
 }
 
